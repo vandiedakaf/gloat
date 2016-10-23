839959d34d9f06c7f39626a018ea2df5e356a8f3
vandiedakaf <vandiedakaf@gmail.com>
2016-10-23 20:30:19 +0200

    Heroku - ClearDB

---
+++ src/main/java/com/vdda/DatabaseConfig.java
@@ -0,0 +1,30 @@
+package com.vdda;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Created by francois on 2016-10-23.
+ */
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    public MysqlDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        MysqlDataSource basicDataSource = new MysqlDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUser(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
