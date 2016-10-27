473ee8a01aaaaf080aceb0c9f0ebabd10001deef
vandiedakaf <vandiedakaf@gmail.com>
2016-10-27 23:58:48 +0200

    Added Jmockit tests and Sonarqube

--- src/test/java/com/vdda/DatabaseConfigTest.java
+++ src/test/java/com/vdda/DatabaseConfigTest.java
@@ -1,16 +1,52 @@
 package com.vdda;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import mockit.*;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by francois on 2016-10-25.
  */
 public class DatabaseConfigTest {
-    @Test
-    public void dataSource() throws Exception {
-//        MysqlDataSource mysqlDataSource = new DatabaseConfig().dataSource();
 
-        // mock new URI(System.getenv("CLEARDB_DATABASE_URL"));
+    @Tested
+    DatabaseConfig databaseConfig;
+
+    @Before
+    public void setUp() throws Exception {
+        databaseConfig = new DatabaseConfig();
+    }
+
+    @Test
+    public void dataSource(@Mocked MysqlDataSource basicDataSource) throws Exception {
+        new MockUp<System>()
+        {
+            @Mock
+            public String getenv(final String string) {
+                return "mysql://user:password@localhost/database?reconnect=true";
+            }
+        };
+
+        MysqlDataSource mysqlDataSource = databaseConfig.dataSource();
+
+        new Verifications() {{
+            basicDataSource.setUrl("jdbc:mysql://localhost/database");
+            basicDataSource.setUser("user");
+            basicDataSource.setPassword("password");
+        }};
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void dataSourceException() throws Exception {
+        new MockUp<System>()
+        {
+            @Mock
+            public String getenv(final String string) {
+                return null;
+            }
+        };
+
+        MysqlDataSource mysqlDataSource = databaseConfig.dataSource();
     }
 }
