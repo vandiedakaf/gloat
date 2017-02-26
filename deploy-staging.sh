d1ca9033224bc7cb23d7bab2a872a60648b1bfb7
vandiedakaf <vandiedakaf@gmail.com>
2017-02-26 13:15:53 +0200

    Pipeline WIP

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -5,7 +5,7 @@ set -e
 docker build -t gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account ${GCLOUD_ACCOUNT} --key-file ${HOME}/gcloud-service-key.json
 
 gcloud --quiet config set project $PROJECT_NAME_STG
 gcloud --quiet config set container/cluster $CLUSTER_NAME_STG
