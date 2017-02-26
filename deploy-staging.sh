16a26be4882ce12498885d986d8921426dd1df02
vandiedakaf <vandiedakaf@gmail.com>
2017-02-26 13:40:53 +0200

    Pipeline WIP

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -2,21 +2,22 @@
 
 set -e
 
-docker build -t gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
+docker build -t gcr.io/${GCLOUD_PROJECT_ID_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode > ${HOME}/gcloud-service-key.json
+cat ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account ${GCLOUD_ACCOUNT} --key-file ${HOME}/gcloud-service-key.json
 
-gcloud --quiet config set project $PROJECT_NAME_STG
-gcloud --quiet config set container/cluster $CLUSTER_NAME_STG
+gcloud --quiet config set project $GCLOUD_PROJECT_ID_STG
+gcloud --quiet config set container/cluster $GCLOUD_CLUSTER_NAME_STG
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
-gcloud --quiet container clusters get-credentials $CLUSTER_NAME_STG
+gcloud --quiet container clusters get-credentials $GCLOUD_CLUSTER_NAME_STG
 
-gcloud docker push gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}
+gcloud docker push gcr.io/${GCLOUD_PROJECT_ID_STG}/${DOCKER_IMAGE_NAME}
 
-yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:latest
+yes | gcloud beta container images add-tag gcr.io/${GCLOUD_PROJECT_ID_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${GCLOUD_PROJECT_ID_STG}/${DOCKER_IMAGE_NAME}:latest
 
 kubectl config view
 kubectl config current-context
 
-kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
+kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${GCLOUD_PROJECT_ID_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
