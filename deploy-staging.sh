6d3ba528eda267ace3e9a9b444572886d130ab85
vandiedakaf <vandiedakaf@gmail.com>
2017-03-23 23:40:20 +0200

    Pipeline WIP

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -21,8 +21,8 @@ gcloud docker push ${DOCKER_IMG_PATH}
 yes | gcloud beta container images add-tag ${DOCKER_IMG_PATH}:$TRAVIS_COMMIT ${DOCKER_IMG_PATH}:latest
 
 echo "K8 SETUP"
-kubectl config view
-kubectl config current-context
+#kubectl config view
+#kubectl config current-context
 
 echo "K8 RUN"
-kubectl run kubernetes-bootcamp --image=${DOCKER_IMG_PATH}:$TRAVIS_COMMIT --port=8080
+#kubectl run kubernetes-bootcamp --image=${DOCKER_IMG_PATH}:$TRAVIS_COMMIT --port=8080
