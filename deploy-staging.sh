e712ac9bbbadf158caf94950493316b7d5bacb2a
Francois du Toit <vandiedakaf@gmail.com>
2017-07-06 00:03:05 +0200

    Starting App Engine Tests

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -14,8 +14,7 @@ echo $GCLOUD_SERVICE_KEY | base64 --decode > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 gcloud --quiet config set project $GCLOUD_PROJECT_ID_STG
-#gcloud --quiet config set container/cluster $GCLOUD_CLUSTER_NAME_STG
 gcloud --quiet config set compute/zone $CLOUDSDK_COMPUTE_ZONE
 
-./gradlew appengineDeploy
+./gradlew appengineDeploy --info
 
