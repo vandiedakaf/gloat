b2d65e0fe968b57cc6635f54e8a5486010b34d59
Francois du Toit <vandiedakaf@gmail.com>
2017-07-06 19:05:46 +0200

    Back to Heroku

---
+++ src/main/java/com/vdda/DatabaseConfig.java
@@ -0,0 +1,33 @@
+package com.vdda;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Created by francois on 2016-10-23 for
+ * vandiedakaf solutions
+ */
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Profile("default")
+    public MysqlDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("JAWSDB_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath() + "?createDatabaseIfNotExist=true";
+
+        MysqlDataSource basicDataSource = new MysqlDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUser(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
